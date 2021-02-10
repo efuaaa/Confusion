450d4f2cadb35a3caa7528efdb70c2014e31cc5d
Ewurafua Plange <plangette19@gmail.com>
2021-02-10 13:25:50 +0000

    Controlled Forms complete

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -14,9 +14,21 @@ class Contact extends Component {
             contactType: 'Tel.',
             message:''
         }
-    }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+        }
 
     handleInputChange(event){
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]:value
+
+        });
 
     }
 
@@ -76,7 +88,8 @@ class Contact extends Component {
                             <Label htmlFor="firstname" md={2}> First Name</Label>
                             <Col md={10}>
                                 <Input type="text" id="firstname" name="firstname" placeholder="First Name"
-                                value={this.state.firstname}/>
+                                value={this.state.firstname}
+                                onChange={this.handleInputChange}/>
                             </Col>
 
                         </FormGroup>
@@ -84,8 +97,9 @@ class Contact extends Component {
                         <FormGroup row>
                             <Label htmlFor="lastname" md={2}> Last Name</Label>
                             <Col md={10}>
-                                <Input type="text" id="lastname" name="lastname" placeholder="First Name"
-                                value={this.state.lastname}/>
+                                <Input type="text" id="lastname" name="lastname" placeholder="Last Name"
+                                value={this.state.lastname}
+                                onChange={this.handleInputChange}/>
                             </Col>
                         </FormGroup>
 
@@ -93,7 +107,8 @@ class Contact extends Component {
                             <Label htmlFor="telnum" md={2}> Contact Tel.</Label>
                             <Col md={10}>
                                 <Input type="text" id="telnum" name="telnum" placeholder="Tel. Number"
-                                value={this.state.telnum}/>
+                                value={this.state.telnum}
+                                onChange={this.handleInputChange}/>
                             </Col>
                         </FormGroup>
 
@@ -101,7 +116,8 @@ class Contact extends Component {
                             <Label htmlFor="email" md={2}> Email</Label>
                             <Col md={10}>
                                 <Input type="email" id="email" name="email" placeholder="Email"
-                                value={this.state.email}/>
+                                value={this.state.email}
+                                onChange={this.handleInputChange}/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
@@ -127,7 +143,8 @@ class Contact extends Component {
                             <Label htmlFor="message" md={2}> Your Feedback</Label>
                             <Col md={10}>
                                 <Input type="textarea" id="message" name="message" rows="12"
-                                value={this.state.message}/>
+                                value={this.state.message}
+                                onChange={this.handleInputChange}/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
